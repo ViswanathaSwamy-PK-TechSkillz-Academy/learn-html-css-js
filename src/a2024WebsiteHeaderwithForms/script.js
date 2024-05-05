a3495ce887332e4477d4fcfb6a830ae1d8a80433
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-06 02:01:06 +0530

    Changes

--- src/a2024WebsiteHeaderwithForms/script.js
+++ src/a2024WebsiteHeaderwithForms/script.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 const joinBtn = document.querySelector(".navigation a:nth-child(3)");
 const homeBtn = document.querySelector(".navigation a:nth-child(2)");
 const loginBtn = document.querySelector(".contact p a");
+const loginMsg = document.querySelector(".contact p span");
 const contactBtn = document.querySelector(".contact-btn:nth-child(2)");
 
 joinBtn.addEventListener("click", () => {
@@ -14,7 +15,6 @@ homeBtn.addEventListener("click", () => {
 
 loginBtn.addEventListener("click", () => {
     container.classList.toggle("hide")
-        ? (loginBtn.textContent = "Sign Up") && (contactBtn.textContent = "Log In")
-        : (loginBtn.textContent = "Log In") &&
-        (contactBtn.textContent = "Create Account");
+        ? (loginBtn.textContent = "Sign Up") && (contactBtn.textContent = "Log In") && (loginMsg.textContent = "Don't have an account?")
+        : (loginBtn.textContent = "Log In") && (contactBtn.textContent = "Create Account") && (loginMsg.textContent = "Already have an account?");
 });
